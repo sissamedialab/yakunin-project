5fcd5cbe144b0c09213426f81a76c71c2ce8659d
Iacopo Spalletti <i.spalletti@nephila.it>
2025-12-12 17:21:27 +0100

    Fix watermark view to support updated websocket-mode

--- yakunin_service/urls.py
+++ yakunin_service/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("test/", views.test_service, name="test"),
     path("mkpdf/", views.mkpdf, name="mkpdf"),
-    path("mkpdf/ws/", views.mkpdf, name="mkpdf"),
+    path("mkpdf/ws/", views.mkpdf, name="mkpdf_ws"),
     path("watermark/", views.watermark, name="watermark"),
-    path("watermark/<slug:feedback_wsname>/", views.watermark, name="watermark"),
+    path("watermark/ws/", views.watermark, name="watermark_ws"),
 ]
